3aa2e12db5541bff7e8b993497691e66c8a2761e
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-06-24 07:32:14 +0100

    [UPDATE] register saves info to database

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 import * as z from "zod";
 import { RegisterSchema } from "@/middleware/schema";
 import { type formFlashProps } from "@/components/auth/formFlash"
+import bcrypt from "bcryptjs";
+import { db, getUserByEmail } from "@/lib/db";
 
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,7 +11,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
     if(!validatedFields) return { type: 'error', message: "Invalid fields!"} as formFlashProps;
 
-    console.log(values);
+    const { username, email, password } = validatedFields.data as typeof values;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser) return { type: 'error', message: "User already exists"} as formFlashProps;
+
+    await db.user.create({
+        data: {
+            name: username,
+            email,
+            password: hashedPassword
+        }
+    })
+
+
+    // TODO: send a verification token email
 
     return { type: 'success', message: "Valid fields"} as formFlashProps;
 }
--- lib/db.ts
+++ lib/db.ts
@@ -7,3 +7,21 @@ declare global {
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db; 
+
+export const getUserByEmail = async (email: string) => {
+	try {
+		const user = await db.user.findUnique({ where: { email } });
+		return user;
+	} catch (error) {
+		return null;
+	}
+}
+
+export const getUserById = async (id: string) => {
+	try {
+		const user = await db.user.findUnique({ where: { id } });
+		return user;
+	} catch (error) {
+		return null;
+	}
+}
